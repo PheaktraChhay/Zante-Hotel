e8b2234cfd5cc556ace5dc3fe0a62d5d559121d3
codingfacilties <120.practice.learning@gmail.com>
2022-03-30 16:58:37 +0700

    30-03-22

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+    var modal = document.getElementById("videoPreview");
+    var btn = document.getElementById("playIcon");
+    var vbg = document.getElementById("vBg");
+    btn.onclick = function () {
+        modal.style.display = "flex";
+        modal.style.alignItems = "center";
+        modal.style.justifyContent = "center";
+        vbg.style.display = "block";
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            vbg.style.display = "none";
+            location.reload();
+        }
+    }
+});
